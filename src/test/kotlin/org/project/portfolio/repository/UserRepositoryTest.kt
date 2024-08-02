59980cccb2aa947ce738cb235a88aa29aa43e4dd
KihaJeong1994 <jkha7371@gmail.com>
2024-08-02 14:00:39 +0900

    UserRepository 추가 및 조회 테스트코드 구현

---
+++ src/test/kotlin/org/project/portfolio/repository/UserRepositoryTest.kt
@@ -0,0 +1,30 @@
+package org.project.portfolio.repository
+
+import org.junit.jupiter.api.Test
+import org.project.portfolio.entity.User
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager
+import org.springframework.data.repository.findByIdOrNull
+import kotlin.test.assertEquals
+
+@DataJpaTest
+class UserRepositoryTest @Autowired constructor(
+    val entityManager: TestEntityManager,
+    val userRepository: UserRepository
+) {
+
+    @Test
+    fun `When findByIdOrNull then return User`() {
+        val user = User("James", "010-1234-5678", "abc@abc.com", "somepassword")
+        entityManager.persist(user)
+        entityManager.flush()
+        entityManager.clear()
+        val result = userRepository.findByIdOrNull(user.id)
+        assertEquals(user.id, result!!.id)
+        assertEquals(user.name, result!!.name)
+        assertEquals(user.phone, result!!.phone)
+        assertEquals(user.email, result!!.email)
+        assertEquals(user.password, result!!.password)
+    }
+}
