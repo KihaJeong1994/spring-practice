7de2b397d9cac0281eecb5753750dccbd65b6ab8
KihaJeong1994 <jkha7371@gmail.com>
2024-08-02 13:58:26 +0900

    영속성 관리를 위한 JPA와 H2 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("io.spring.dependency-management") version "1.1.5"
     kotlin("jvm") version "1.9.24"
     kotlin("plugin.spring") version "1.9.24"
+    kotlin("plugin.jpa") version "1.9.24"
 }
 
 group = "org.project"
@@ -22,9 +23,17 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.springframework.data:spring-data-envers") // jpa + audit(version 기록?)
+    implementation("org.springframework.boot:spring-boot-starter-jdbc") // 빼먹으면 application.yaml에 있는 datasource, jpa 설정 안가져옴. 그럴경우 직접 Config 작성해주어야함
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    runtimeOnly("com.h2database:h2")
+    // Lombok 라이브러리
+    compileOnly("org.projectlombok:lombok:1.18.26")
+    annotationProcessor("org.projectlombok:lombok:1.18.26")
+
 }
 
 kotlin {
